b3c1ffb71eb9ea6d7cce5ea77b40cf2dbc880ae8
Andre Rizaldi Brillianto <andrerizaldib@gmail.com>
2023-11-27 16:20:23 +0700

    create FoodieHub, Android App

---
+++ app/src/main/java/com/codelamps/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.codelamps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
