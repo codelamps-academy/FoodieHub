0878fefeaeaf0a37152a7ea3c685e9783976d20c
Codelamps <andrerizaldib@gmail.com>
2023-11-28 09:51:04 +0700

    create activity_login, activity_start, splash_screen, and signup_activity

--- app/src/main/java/com/codelamps/SplashScreen.kt
+++ app/src/main/java/com/codelamps/SplashScreen.kt
@@ -12,7 +12,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
--- app/src/main/java/com/codelamps/StartActivity.kt
+++ app/src/main/java/com/codelamps/StartActivity.kt
@@ -1,11 +1,22 @@
 package com.codelamps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val nextButton = findViewById<Button>(R.id.nextButton)
+
+        nextButton.setOnClickListener {
+            // intent ke login
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
